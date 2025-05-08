1c98dcd292872b95a6f9cce7fc09caee33797644
Shinzaburo <zaburo.0316@icloud.com>
2025-05-09 00:32:15 +0900

    フォント変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import { Container } from "@mui/material";
+// src/App.tsx
+import { Container, CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import React from "react";
@@ -6,20 +8,24 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Header from "./common/Header";
 import Calendar from "./pages/Calender/Calender";
 import TimeRecord from "./pages/TimeRecord/TimeRecord";
+import theme from "./theme/theme";
 
 const App: React.FC = () => {
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <Router>
-        <Header />
-        <Container maxWidth="sm" sx={{ mt: 5 }}>
-          <Routes>
-            <Route path="/" element={<TimeRecord />} />
-            <Route path="/calendar" element={<Calendar />} />
-          </Routes>
-        </Container>
-      </Router>
-    </LocalizationProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Router>
+          <Header />
+          <Container maxWidth="sm" sx={{ mt: 5 }}>
+            <Routes>
+              <Route path="/" element={<TimeRecord />} />
+              <Route path="/calendar" element={<Calendar />} />
+            </Routes>
+          </Container>
+        </Router>
+      </LocalizationProvider>
+    </ThemeProvider>
   );
 };
 
---
+++ src/theme/theme.ts
@@ -0,0 +1,10 @@
+import { createTheme } from "@mui/material/styles";
+
+// メイリオフォントを設定したテーマ
+const theme = createTheme({
+  typography: {
+    fontFamily: '"Meiryo", "Hiragino Kaku Gothic ProN", "Helvetica", "Arial", sans-serif',
+  },
+});
+
+export default theme;
